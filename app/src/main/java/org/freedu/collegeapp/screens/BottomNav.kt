fce9e4be5de929c31656a1cdb12b17bd88270632
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2024-05-16 22:29:32 +0600

    Bottom navigation added

--- app/src/main/java/org/freedu/collegeapp/screens/BottomNav.kt
+++ app/src/main/java/org/freedu/collegeapp/screens/BottomNav.kt
@@ -2,11 +2,12 @@ package org.freedu.collegeapp.screens
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,6 +15,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,10 +32,16 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.launch
 import org.freedu.collegeapp.R
+import org.freedu.collegeapp.models.BottomNavItem
 import org.freedu.collegeapp.models.NavItem
+import org.freedu.collegeapp.navigation.Routes
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -109,13 +117,80 @@ fun BottomNav(navController: NavController) {
                                 Icon(imageVector = Icons.Rounded.Menu, contentDescription = null)
                             }
                         })
-                }
+                },
+                bottomBar = { MyBottomNav(navController = navController1) }
             ) { padding ->
-                Column(modifier = Modifier.padding(padding)) {
-
+                NavHost(navController = navController1, startDestination = Routes.Home.route, modifier = Modifier.padding(padding)) {
+                    composable(route = Routes.Home.route) {
+                        Home()
+                    }
+                    composable(route = Routes.Gallery.route){
+                        Gallery()
+                    }
+                    composable(route = Routes.AboutUs.route){
+                        AboutUs()
+                    }
+                    composable(route = Routes.Faculty.route){
+                        Faculty()
+                    }
                 }
 
             }
         }
     )
+}
+
+@Composable
+fun MyBottomNav(navController: NavController) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+
+    val list = listOf(
+        BottomNavItem(
+            "Home",
+            R.drawable.home,
+            Routes.Home.route
+        ),
+        BottomNavItem(
+            "Faculty",
+            R.drawable.graduated,
+            Routes.Faculty.route
+        ),
+        BottomNavItem(
+            "Gallery",
+            R.drawable.gallery,
+            Routes.Gallery.route
+        ),
+        BottomNavItem(
+            "About",
+            R.drawable.info,
+            Routes.AboutUs.route
+        )
+
+    )
+
+
+    BottomAppBar {
+        list.forEach {
+            val current = it.route
+            val otherRoute = try {
+                backStackEntry.value!!.destination.route
+            } catch (e: Exception) {
+                Routes.Home.route
+            }
+
+            val selected = current == otherRoute
+
+            NavigationBarItem(selected = selected, onClick = {
+                navController.navigate(it.route) {
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                }
+            }, icon = {
+                Icon(painter = painterResource(id = it.icon), contentDescription = it.title, modifier = Modifier.size(24.dp))
+            })
+
+        }
+    }
 }
